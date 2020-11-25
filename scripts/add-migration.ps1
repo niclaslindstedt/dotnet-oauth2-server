e4e2c547fd873fd0c63fdefb77049072b8b06e35
Niclas Lindstedt <nicl@slindstedt.se>
2020-11-25 21:03:12 +0100

    Add database scripts

---
+++ scripts/add-migration.ps1
@@ -0,0 +1,4 @@
+$cwd = Get-Location
+Set-Location $PSScriptRoot\.. | Out-Null
+dotnet ef migrations add $args --project Etimo.Id.Data --startup-project Etimo.Id.Api
+Set-Location $cwd | Out-Null
---
+++ scripts/update.ps1
@@ -0,0 +1,4 @@
+$cwd = Get-Location
+Set-Location $PSScriptRoot\.. | Out-Null
+dotnet ef database update --project Etimo.Id.Data --startup-project Etimo.Id.Api
+Set-Location $cwd | Out-Null
